4eaf158b3e035131dc25c91cad318723ac9564aa
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2026-01-21 13:28:30 +0100

    [ADD] proto conversion methods and update config fields

--- application/backend/pkg/config/config.go
+++ application/backend/pkg/config/config.go
@@ -9,6 +9,7 @@ import (
 	"github.com/valentin-kaiser/go-core/config"
 	"github.com/valentin-kaiser/go-core/flag"
 	"github.com/valentin-kaiser/go-core/logging/log"
+	"github.com/valentin-kaiser/hdns/pkg/proto/service"
 )
 
 type App struct {
@@ -19,10 +20,12 @@ type App struct {
 type Service struct {
 	LogLevel        int      `usage:"(0 = debug, 1 = info, 2 = warn, 3 = error, 4 = fatal, 5 = panic)" json:"log_level"`
 	WebPort         int16    `usage:"port to bind the web server to" json:"web_port"`
-	CertificateFile string   `usage:"path to the TLS certificate file" json:"certificate_file"`
-	KeyFile         string   `usage:"path to the TLS key file" json:"key_file"`
+	CertificatePath string   `usage:"path to the TLS certificate file" json:"certificate_file"`
+	KeyPath         string   `usage:"path to the TLS key file" json:"key_file"`
 	RefreshCron     string   `usage:"cron expression to schedule data refresh tasks" json:"refresh_cron"`
 	DNSServers      []string `usage:"list of DNS servers to use for lookups" json:"dns_servers"`
+	IPv4Resolvers   []string `usage:"list of IPv4 resolvers to determine public IP address" json:"ipv4_resolvers"`
+	IPv6Resolvers   []string `usage:"list of IPv6 resolvers to determine public IP address" json:"ipv6_resolvers"`
 }
 
 type Database struct {
@@ -38,10 +41,27 @@ func Init() {
 		Service: Service{
 			LogLevel:        1,
 			WebPort:         9100,
-			CertificateFile: filepath.Join(flag.Path, "certs/hdns.cert"),
-			KeyFile:         filepath.Join(flag.Path, "certs/hdns.key"),
-			RefreshCron:     "@every 5m",
-			DNSServers:      []string{"9.9.9.9:53", "1.1.1.1:53", "8.8.8.8:53"},
+			CertificatePath: filepath.Join(flag.Path, "certs/hdns.cert"),
+			KeyPath:         filepath.Join(flag.Path, "certs/hdns.key"),
+			RefreshCron:     "0 */5 * * * *",
+			DNSServers: []string{
+				"9.9.9.9:53",
+				"1.1.1.1:53",
+				"8.8.8.8:53",
+			},
+			IPv4Resolvers: []string{
+				"https://nms.intellitrend.de",
+				"https://api.ipify.org",
+				"https://api.my-ip.io/ip",
+				"https://api.ipy.ch",
+				"https://ident.me/",
+				"https://ifconfig.me/ip",
+				"https://icanhazip.com/",
+			},
+			IPv6Resolvers: []string{
+				"https://api6.ipify.org",
+				"https://ipv6.icanhazip.com",
+			},
 		},
 		Database: Database{
 			Host:     "localhost",
@@ -141,4 +161,46 @@ func (c *Database) Validate() error {
 	}
 
 	return nil
+}
+
+func (c *App) ToProto() *service.Configuration {
+	return &service.Configuration{
+		Service: &service.Service{
+			LogLevel:        int32(c.Service.LogLevel),
+			WebPort:         int32(c.Service.WebPort),
+			CertificatePath: c.Service.CertificatePath,
+			KeyPath:         c.Service.KeyPath,
+			RefreshCron:     c.Service.RefreshCron,
+			DnsServers:      c.Service.DNSServers,
+		},
+		Database: &service.Database{
+			Host:     c.Database.Host,
+			Port:     uint32(c.Database.Port),
+			Username: c.Database.Username,
+			Password: c.Database.Password,
+			Name:     c.Database.Name,
+		},
+	}
+}
+
+func (c *App) FromProto(pc *service.Configuration) *App {
+	if pc == nil {
+		return nil
+	}
+	if pc.Service == nil || pc.Database == nil {
+		return nil
+	}
+
+	c.Service.LogLevel = int(pc.Service.LogLevel)
+	c.Service.WebPort = int16(pc.Service.WebPort)
+	c.Service.CertificatePath = pc.Service.CertificatePath
+	c.Service.KeyPath = pc.Service.KeyPath
+	c.Service.RefreshCron = pc.Service.RefreshCron
+	c.Service.DNSServers = pc.Service.DnsServers
+	c.Database.Host = pc.Database.Host
+	c.Database.Port = uint16(pc.Database.Port)
+	c.Database.Username = pc.Database.Username
+	c.Database.Password = pc.Database.Password
+	c.Database.Name = pc.Database.Name
+	return c
 }
